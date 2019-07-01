b6078fe68ee549494ff76585c3a84cb1f71b5ca2
Nikita <46563672+V1be-Official@users.noreply.github.com>
2019-07-01 22:05:52 +0300

    fix bugs

--- IT-Academy/Tasks/A/A4+/app.js
+++ IT-Academy/Tasks/A/A4+/app.js
@@ -4,16 +4,19 @@ function checkPalindrom(str) {
     str = str.toLowerCase();
     str = str.replace(/[.,\/#!?$%ьъ\^&\*;:{}=\-_`~()\s]/g,"");
     str = str.replace(/ё/g,"е");
-    function recurs(str, k) {
+    function recurs(k) {
         if(k == Math.round(str.length/2)) {
             return true;
         }  
         if(str[k] == str[str.length-1-k]) {
-            return recurs(str, k + 1);
+            return recurs(k + 1);
         }
         else
             return false;
     }
-    return recurs(str, 0);
+    return recurs(0);
 }
-console.log(checkPalindrom(str));
+if(checkPalindrom(str)) {
+    alert("Да");
+} else
+    alert("Нет");
