b8b41fe7596a9192acb801c90345a2e983b9501d
V1be-Official <vibegame@yandex.by>
2019-07-10 20:03:15 +0300

    add React

---
+++ Learning-React/2019-07-10/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Info from './components/info';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>hello</div>
+    );
+  }
+}
+
+export default App;
---
+++ Learning-React/2019-07-10/src/components/info.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Info extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Приложение о погоде</h2>
+                <p>Узнайте погоду в вашем городе</p>
+            </div>
+        );
+    }
+}
+export default Info;
