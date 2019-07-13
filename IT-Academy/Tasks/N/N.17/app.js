7fb81d8d25d6acb2a520f39e2ef2aec538a93823
V1be-Official <vibegame@yandex.by>
2019-07-13 22:23:15 +0300

    fix bugs and add new tasks

---
+++ IT-Academy/Tasks/N/N.17/app.js
@@ -0,0 +1,46 @@
+function howManyVowels(func, str) {
+    let vowels = {
+        "а": 0,
+        "о": 0,
+        "э": 0,
+        "ы": 0,
+        "и": 0,
+        "у": 0,
+        "я": 0,
+        "ё": 0,
+        "ю": 0,
+        "е": 0,
+    };
+    str = str.toLowerCase();
+
+    function solveThroughFilter() {
+        function onlyVowels(v) {
+            return (v in vowels);
+        }
+        return str.split('').filter(onlyVowels).length;
+    }
+
+    function solveThroughForEach() {
+        let result = 0;
+        function onlyVowels(v) {
+            result += (v in vowels);
+        }
+        str.split('').forEach(onlyVowels);
+        return result;
+    }
+
+    function solveThroughReduce() {
+        function onlyVowels(r, v) {
+            return r + (v in vowels); 
+        }
+        return str.split('').reduce(onlyVowels, 0);
+    }
+    switch(func) {
+        case 'filter': return solveThroughFilter();
+        case 'forEach': return solveThroughForEach();
+        case 'reduce': return solveThroughReduce();
+    }
+}
+console.log(howManyVowels("forEach", "АоАооБгнрпЕеЯя"));
+console.log(howManyVowels("filter", "АоАооБгнрпЕеЯя"));
+console.log(howManyVowels("reduce", "АоАооБгнрпЕеЯя"));
