30e3e9eed835fb7275088c2d58c14836e78d06ca
Nikita <46563672+V1be-Official@users.noreply.github.com>
2019-11-14 23:32:46 +0300

    add

---
+++ IT-Academy/JS/Tasks/T/Checkbox/app.js
@@ -0,0 +1,6 @@
+let elem = document.getElementById('acc');
+let aim = document.getElementById('svg');
+elem.addEventListener('click', () => {
+    console.log('dsadas');
+    aim.classList.toggle('active');
+})
