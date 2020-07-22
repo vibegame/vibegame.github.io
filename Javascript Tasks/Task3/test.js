586a5a6a3cda469e2b6ec3ecaf86eb035aeac16e
Nikita <46563672+V1be-Official@users.noreply.github.com>
2020-07-22 17:52:59 +0300

    d

---
+++ Javascript Tasks/Task3/test.js
@@ -0,0 +1,29 @@
+class QuadraticEquation {
+    a = null;
+
+    b = null;
+
+    c = null;
+
+    constructor(a, b, c) {}
+
+    checkForNumber() {}
+
+    resolve() {}
+}
+
+const quadraticEquation1 = new QuadraticEquation(3, -1, -2);
+
+quadraticEquation1.resolve(); // [1, -2/3]
+
+const quadraticEquation2 = new QuadraticEquation(5, 3, 1);
+
+quadraticEquation2.resolve(); // null
+
+const quadraticEquation2 = new QuadraticEquation(5, 3, 1);
+
+quadraticEquation2.resolve(); // [-0.5]
+
+
+
+
